1dc8b9e391f7489e16dc82e0fce6407fe74674a4
jorden134677 <48340565+jorden134677@users.noreply.github.com>
2019-04-09 20:21:50 +0800

    Add files via upload

---
+++ basic08.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+#include<conio.h> 
+
+
+int main() 
+{ 
+
+int i,j,s,h=0; 
+
+printf("輸入n，顯示到n的質數:： "); 
+scanf("%d",&s); 
+
+for ( i = 2 ; i <= s ; i++) 
+{ 
+h =0; 
+
+for ( j = 1 ; j <= s ; j++) 
+{ 
+if( (i % j) == 0 ) h++; 
+if ( h > 2 ) break; 
+} 
+if ( h <= 2 ) printf("%d \t 是質數\n",i); 
+
+} 
+getch(); 
+return 0; 
+
+}
